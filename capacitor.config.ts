c409f6dba91fa8970963e45d4d50edc10b761b1d
Panha <thopanha123@gmail.com>
2026-06-29 21:01:21 +0700

    compiler web to mobile app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.panhadev.teamyearng.app',
+  appName: 'PanhaProfile',
+  webDir: 'dist'
+};
+
+export default config;
